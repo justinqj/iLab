09a58a3664319a43377ceab84a9945dc1b533c5c
Quadradrop <115875710+Quadradrop@users.noreply.github.com>
2023-09-07 10:56:28 +1000

    Update patents.py

--- patents.py
+++ patents.py
@@ -1,49 +1,53 @@
 import requests
 import time
-url = 'https://api.lens.org/patent/search'
+import json
 
-# include fields
-include = '''["biblio.invention_title", "biblio.parties.inventors.extracted_name", "date_published"]'''
-# request body with scroll time of 1 minute
-request_body = '''{
+API_URL = 'https://api.lens.org/patent/search'
+BATCH_SIZE = 100
+
+DELAY = 1
+OUTPUT_FILE = "output.json"
+
+include = ["biblio.invention_title", "biblio.parties.inventors.extracted_name", "date_published"]
+headers = {'Authorization': 'Bearer 02w63YLQJJRT3i8THxetpAJCvEO1883fymZIXxOzK2gP1N6UK3SN', 'Content-Type': 'application/json'}
+
+all_results = []
+offset = 0
+while True:
+  request_body = {
     "query": {
         "match":  {
             "title": "technology"
         }
     },
-    "include": %s,
-    "scroll": "1m",
-    "size": 20,
+    "include": include,
+    "from": offset,
+    "size": BATCH_SIZE,
     "sort": [{"date_published": "desc"}]
-}''' % include
-headers = {'Authorization': 'Bearer 02w63YLQJJRT3i8THxetpAJCvEO1883fymZIXxOzK2gP1N6UK3SN', 'Content-Type': 'application/json'}
+  }
+  response = requests.post(API_URL, json=request_body, headers=headers)
 
-# Recursive function to scroll through paginated results
-def scroll(scroll_id):
-  # Change the request_body to prepare for next scroll api call
-  # Make sure to append the include fields to make faster response
-  if scroll_id is not None:
-    global request_body
-    request_body = '''{"scroll_id": "%s", "include": %s}''' % (scroll_id, include)
-
-  # make api request
-  response = requests.post(url, data=request_body, headers=headers) 
-
-  # If rate-limited, wait for n seconds and proceed the same scroll id
-  # Since scroll time is 1 minutes, it will give sufficient time to wait and proceed
   if response.status_code == requests.codes.too_many_requests:
-    time.sleep(8)
-    scroll(scroll_id)
-  # If the response is not ok here, better to stop here and debug it
-  elif response.status_code != requests.codes.ok:
-    print(response.json())
-  # If the response is ok, do something with the response, take the new scroll id and iterate
-  else:
-    json = response.json()
-    if json.get('results') is not None and json['results'] > 0:
-        scroll_id = json['scroll_id'] # Extract the new scroll id from response
-        print(json['data']) #DO something with your data
-        scroll(scroll_id)
+    print(f"[ERROR]: We are being rate limited, sleeping for {DELAY} and trying again.")
+    time.sleep(DELAY)
+    continue
 
-# start recursive scrolling
-scroll(scroll_id=None)
+  elif response.status_code != requests.codes.ok:
+    print(f"[ERROR]: Request returned code {response.status_code}, aborting. Response was:")
+    print(response.json())
+    break
+
+  else:
+    response_json = response.json()
+    results = response_json.get('data', [])
+    total = response_json.get('total', 0)
+    all_results.extend(results)
+    offset += BATCH_SIZE
+
+    if offset >= total:
+      break
+    print(f"[INFO] Got {len(results)}, currently: {offset} / {total} results")
+
+print(f"[INFO] Writing {len(all_results)} results to output file: {OUTPUT_FILE}")
+with open(OUTPUT_FILE, "w") as f:
+  f.write(json.dumps(all_results))
